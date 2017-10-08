76124d45f855d3d7194c1de51133ca1b3f4b35ee
root <root@localhost.localdomain>
2017-10-08 10:59:14 -0400

    update

--- sysbench_fileio.sh
+++ sysbench_fileio.sh
@@ -3,22 +3,68 @@
 
 REPORT_INTERVAL=3
 MAX_TIME=15
+FILE_TOTAL_SIZE=2
+FILE_NUM=4
 
-#~/sb_iotest/prepare.sh
-#### PREPARE #######
-
-
-
+function prepare {
+    echo PREPRAE start....
+    sysbench --test=fileio cleanup
+    sysbench --test=fileio --file-num=$FILE_NUM --file-total-size=${FILE_TOTAL_SIZE}G prepare
+    echo PREPRAE done....
+}
 
 ############ different threads #############
-sysbench --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 --file-block-size=16k --test=fileio --num-threads=16 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndrd run
-sysbench --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 --file-block-size=16k --test=fileio --num-threads=8 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndrd run
-sysbench --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 --file-block-size=16k --test=fileio --num-threads=4 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndrd run
-sysbench --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 --file-block-size=16k --test=fileio --num-threads=2 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndrd run
-sysbench --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 --file-block-size=16k --test=fileio --num-threads=1 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndrd run
-#sysbench --max-time=30 --max-requests=1000000 --file-block-size=16k --test=fileio --num-threads=16 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndwr run
+function run_thread {
+    
+    for THREAD_NUM in 1 2 4 8 16 32
+    do
+        echo threads: $THREAD_NUM
+        sysbench --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 \
+        --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=16k \
+        --file-extra-flags=direct --file-test-mode=rndrd run
+    done
+}
 
 
-#sysbench --max-time=30 --max-requests=1000000 --file-block-size=8k --test=fileio --num-threads=16 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndrd run
-#sysbench --max-time=30 --max-requests=1000000 --file-block-size=8k --test=fileio --num-threads=16 --file-extra-flags=direct --file-total-size=1G --file-test-mode=rndwr run
+
+
+############ different block size #############
+function run_block {
+    THREAD_NUM=1
+    for FILE_BLOCK_SIZE in 1 2 4 8 16 32
+    do
+        echo threads: $THREAD_NUM
+        sysbench --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 \
+        --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=${FILE_BLOCK_SIZE}k \
+        --file-extra-flags=direct --file-test-mode=rndrd run
+    done
+}
+
+if [ $# -eq 1 ] && [ $1 = "prepare" ] # re-prepare or not?
+then
+    prepare
+elif [ $# -ne 2]
+then
+    echo Usage: $0 [prepare/run/all] [thread/block/...]
+fi
+
+if [ $1 = "all" ]
+then
+    prepare
+fi
+
+if [ $2 = "thread" ]
+then
+    run_thread
+elif [ $2 = "block" ]
+then
+    run_block
+else
+    echo $2 is an illegal parameter
+fi
+
+
+
+
+echo done....
 
