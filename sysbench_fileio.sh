dc78ed1db03da7d84fdf05e88ac48380729b3523
ZhangJaycee <zhjcyx@gmail.com>
2017-11-18 16:40:48 +0800

    update

--- sysbench_fileio.sh
+++ sysbench_fileio.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-SYSBENCH_PATH=sysbench
+SYSBENCH_PATH=/home/zjc/bin/sysbench-1.0/bin/sysbench
 
 REPORT_INTERVAL=5
 MAX_TIME=50
-FILE_TOTAL_SIZE=16
-FILE_NUM=4
+FILE_TOTAL_SIZE=20
+FILE_NUM=1
 
 function prepare {
     echo PREPRAE start....
@@ -37,32 +37,42 @@ function run_block {
     done
 }
 
-function run_full_throughput {
+function run_bandwidth {
+    THREAD_NUM=16
+    FILE_BLOCK_SIZE=64
+    echo "full throughput"
+    echo "block_size: ${FILE_BLOCK_SIZE}kB thread_num: $THREAD_NUM"
+    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000000 \
+    --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=${FILE_BLOCK_SIZE}k \
+    --file-extra-flags=direct --file-test-mode=seqrd run
+}
+
+function run_bandwidth_write {
     THREAD_NUM=4
     FILE_BLOCK_SIZE=8192
     echo "full throughput"
     echo "block_size: ${FILE_BLOCK_SIZE}kB thread_num: $THREAD_NUM"
-    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 \
+    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000000 \
+    --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=${FILE_BLOCK_SIZE}k \
+    --file-extra-flags=direct --file-test-mode=seqwr run
+}
+
+function run_iops {
+    THREAD_NUM=64
+    FILE_BLOCK_SIZE=4
+    echo "full CPU"
+    echo "block_size: ${FILE_BLOCK_SIZE}kB thread_num: $THREAD_NUM"
+    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000000 \
     --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=${FILE_BLOCK_SIZE}k \
     --file-extra-flags=direct --file-test-mode=rndrd run
 }
 
-function run_full_throughput_write {
-    THREAD_NUM=4
-    FILE_BLOCK_SIZE=8192
-    echo "full throughput"
-    echo "block_size: ${FILE_BLOCK_SIZE}kB thread_num: $THREAD_NUM"
-    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 \
-    --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=${FILE_BLOCK_SIZE}k \
-    --file-extra-flags=direct --file-test-mode=rndwr run
-}
-
-function run_full_cpu {
-    THREAD_NUM=128
+function run_latency_sensitive {
+    THREAD_NUM=1
     FILE_BLOCK_SIZE=4
     echo "full CPU"
     echo "block_size: ${FILE_BLOCK_SIZE}kB thread_num: $THREAD_NUM"
-    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000 \
+    $SYSBENCH_PATH --test=fileio --max-time=$MAX_TIME --report-interval=$REPORT_INTERVAL --max-requests=1000000000 \
     --num-threads=$THREAD_NUM --file-total-size=${FILE_TOTAL_SIZE}G --file-num=$FILE_NUM --file-block-size=${FILE_BLOCK_SIZE}k \
     --file-extra-flags=direct --file-test-mode=rndrd run
 }
@@ -90,15 +100,18 @@ then
 elif [ $2 = "block" ]
 then
     run_block
-elif [ $2 = "full_io" ]
+elif [ $2 = "bw" ]
 then
-    run_full_throughput
-elif [ $2 = "full_io_wr" ]
+    run_bandwidth
+elif [ $2 = "bw_wr" ]
 then
-    run_full_throughput_write
-elif [ $2 = "full_cpu" ]
+    run_bandwidth_write
+elif [ $2 = "iops" ]
 then
-    run_full_cpu
+    run_iops
+elif [ $2 = "lat" ]
+then
+    run_latency_sensitive
 else
     echo $2 is an illegal parameter
 fi
---
+++ sysbench_memory.sh
@@ -0,0 +1,17 @@
+# memory options:
+#   --memory-block-size=SIZE    size of memory block for test [1K]
+#   --memory-total-size=SIZE    total size of data to transfer [100G]
+#   --memory-scope=STRING       memory access scope {global,local} [global]
+#   --memory-hugetlb=[on|off]   allocate memory from HugeTLB pool [off]
+#   --memory-oper=STRING        type of memory operations {read, write, none} [write]
+#   --memory-access-mode=STRING memory access mode {seq,rnd} [seq]
+
+#SYSBENCH_BIN=/home/zjc/bin/sysbench/bin/sysbench
+SYSBENCH_BIN=/home/zjc/bin/sysbench-1.0/bin/sysbench
+
+
+# Throughput
+${SYSBENCH_BIN} --threads=16 --time=30 --test=memory --memory-block-size=64K --memory-total-size=200G --memory-access-mode=rnd --memory-oper=read run
+
+# Latency
+${SYSBENCH_BIN} --threads=1 --time=30 --test=memory --memory-block-size=4K --memory-total-size=20G --memory-access-mode=rnd --memory-oper=read run
