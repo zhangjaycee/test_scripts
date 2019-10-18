8b43218d2704e80f9fee20f8b26e87bf0945cdbc
Jaycee Zhang <zhjcyx@gmail.com>
2019-10-18 16:13:59 +0800

    update pmem fio scripts

--- fio/pmem/start.sh
+++ fio/pmem/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-fio mmap.fio
-fio mmap_wr.fio
+fio mmap_lat.fio
+fio mmap_lat_wr.fio
 fio mmap_bw.fio
 fio mmap_bw_wr.fio
+fio mmap.fio
+fio mmap_wr.fio
--- fio/pmem/start_pvsync.sh
+++ fio/pmem/start_pvsync.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-fio pvsync.fio
-fio pvsync_wr.fio
-fio pvsync_bw.fio
-fio pvsync_bw_wr.fio
+FIO_BIN=/home/zjc/bin/fio/bin/fio
+
+$FIO_BIN pvsync_lat.fio
+$FIO_BIN pvsync_lat_wr.fio
+$FIO_BIN pvsync.fio
+$FIO_BIN pvsync_wr.fio
+$FIO_BIN pvsync_bw.fio
+$FIO_BIN pvsync_bw_wr.fio
