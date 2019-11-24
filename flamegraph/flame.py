d4285b1586ac7dfeeab3007416050be7b9ccc9d7
Jaycee Zhang <zhjcyx@gmail.com>
2019-11-24 15:24:48 +0800

    update the flame.py scripts for generality

--- flamegraph/flame.py
+++ flamegraph/flame.py
@@ -12,7 +12,7 @@ will generate "cpu-flame-nblucene-HDD-100secs.svg" and "io-flame-nblucene-HDD-10
 '''
 
 TEST_CPU = True
-TEST_IO = False
+TEST_IO = True
 
 DROPCACHE_INTERVAL = 5
 
@@ -38,14 +38,15 @@ if os.geteuid() != 0:
     print "This program must be run as root. Aborting."
     sys.exit(1)
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print "Wrong parameter number:"
-    print "\tusage:", sys.argv[0], "<seconds> <identifer>"
+    print "\tusage:", sys.argv[0], "<seconds> <identifer> <program_name>"
     sys.exit(1)
 
 test_time = sys.argv[1]
 identifer = sys.argv[2]
-pid = check_output(["pidof","cl_searching"]).split()[0]
+program = sys.argv[3]
+pid = check_output(["pidof", program]).split()[0]
 
 # on-CPU flame graph commands:
 cmd1 = "perf record -F 99 -g -p " + pid + " sleep " + test_time
@@ -56,18 +57,20 @@ cpu_th = threading.Thread(target = th_shell, args = (cmd_cpu, "<CPU> ", ))
 
 # off-CPU flame graph commands:
 cmd1 = "./fileiostacks.py -f " + test_time + " > out.stacks"
-cmd2 = "grep cl_searching out.stacks | ./flamegraph.pl --width 1400 --color=io --title=\"File I/O Time Flame Graph\" --countname=us > io-flame-" + identifer + "-" + test_time +"secs" + ".svg"
+cmd2 = "grep " + program + " out.stacks | ./flamegraph.pl --width 1400 --color=io --title=\"File I/O Time Flame Graph\" --countname=us > io-flame-" + identifer + "-" + test_time +"secs" + ".svg"
 cmd_io = [cmd1, cmd2]
 io_th = threading.Thread(target = th_shell, args = (cmd_io, "<IO>  ", ))
 
-# drop page cache first
+'''
+# drop page cache periodly
 with open('/proc/sys/vm/drop_caches', 'w') as stream:
     stream.write("1\n");
 
 dropcache_th = threading.Thread(target = th_dropcache)
+dropcache_th.start()
+'''
 
 # start testing
-dropcache_th.start()
 if TEST_CPU:
     cpu_th.start()
 if TEST_IO:
